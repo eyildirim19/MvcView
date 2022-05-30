b94cb3bb559168e9dd8dafbada037f7e90807a24
BTAkademimcsd <53770975+BTAkademimcsd@users.noreply.github.com>
2022-05-30 21:03:39 +0300

    Add project files.

---
+++ MvcView/Controllers/DigerController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MvcView.Controllers
+{
+	public class DigerController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		public IActionResult Sayfa2()
+		{
+			return View("Tablo");
+		}
+	}
+}
---
+++ MvcView/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MvcView.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        //IActionResult
+
+        // ViewResult => Action metodun geriye view dönmesi için kullanılan result type'tır.. IActionResult interface'inden türemiştir...
+
+        public ViewResult Index()
+        {
+            return View();
+        }
+
+
+    }
+}
